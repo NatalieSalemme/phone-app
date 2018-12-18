3f0ac94a977f48cf7c33eb4b3477132ad297b185
Natalie Salemme <NatalieSalemme@gmail.com>
2018-12-17 22:37:37 -0800

    adding FAQs

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,53 +3,46 @@ import { PanelGroup, Panel } from 'react-bootstrap';
 
 const FAQ = () => (
   <div>
+    <h1 className="faq-header"> FAQs</h1>
   <PanelGroup accordion id="accordion-example">
     <Panel eventKey="1">
       <Panel.Heading>
-        <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
+        <Panel.Title toggle>Does I-C-It cost money to use?</Panel.Title>
       </Panel.Heading>
-      <Panel.Body collapsible>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus
-        labore sustainable VHS.
+      <Panel.Body collapsible >
+      Currently, all features are available to our users for free. We do not plan on charging our users for any sort of premium content. We are lucky to be able to maintain our website due to the support of our sponsors.
       </Panel.Body>
     </Panel>
     <Panel eventKey="2">
       <Panel.Heading>
-        <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
+        <Panel.Title toggle>Do you sell our personal information?</Panel.Title>
       </Panel.Heading>
       <Panel.Body collapsible>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus
-        labore sustainable VHS.
+        We respect our users' privacy and would never sell any of our users' information to 3rd party businessess. All your information that you put up on your profile is considered your own property and you will not have to worry about anything being sold or sent to someone else.
       </Panel.Body>
     </Panel>
     <Panel eventKey="3">
       <Panel.Heading>
-        <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
+        <Panel.Title toggle>How many profiles can I use?</Panel.Title>
       </Panel.Heading>
       <Panel.Body collapsible>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus
-        labore sustainable VHS.
+        You are able to have as many profiles as you would like, the possibilities are endless. For organizational purposes, if you have an empty group profile with no connections on it for 90 consecutive days, we will send you a reminder to delete or keep it.
+      </Panel.Body>
+    </Panel>
+    <Panel eventKey="4">
+      <Panel.Heading>
+        <Panel.Title toggle>What do I do if I forgot my password?</Panel.Title>
+      </Panel.Heading>
+      <Panel.Body collapsible>
+        On the login page, you will be able to see a "forgot password". It will send an e-mail to the e-mail on file and you will be able to reset your password from there.
+      </Panel.Body>
+    </Panel>
+    <Panel eventKey="5">
+      <Panel.Heading>
+        <Panel.Title toggle>How much currency can I exchange?</Panel.Title>
+      </Panel.Heading>
+      <Panel.Body collapsible >
+        You are able to exchange up to $6000 USD per month. Before you are able to exchange, we require that you confirm your identity. Then we will send a small amount to your currency wallet so you can verify the amount. Once that is complete(typically within 24 hours), we will send you an e-mail.
       </Panel.Body>
     </Panel>
   </PanelGroup>
